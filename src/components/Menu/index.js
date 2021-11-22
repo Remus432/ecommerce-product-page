28b958c7c9b40bc7207a8f4bf2627d7b00d19bfc
remus432 <brosdev1@gmail.com>
2021-11-22 11:26:19 +0100

    commited

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -26,28 +26,24 @@ const Menu = () => {
     <div className="menu">
       <img onClick={() => openMenu()} src={HamburgerMenuIcon} alt="Hamburger Menu" className="menu__hamburger" />
       <img className="menu__logo" alt="Sneakers Logo" src={Logo} />
-      {
-        isMenuOpen && (
-          <ul className="menu__list">
-            <img onClick={() => closeMenu()} className="menu__close" src={MenuCloseIcon} alt="Menu Close Icon" />
-            <li className="menu__item">
-              <a href="#2" className="menu__item-link">Collections</a>
-            </li>
-            <li className="menu__item">
-              <a href="#1" className="menu__item-link">Men</a>
-            </li>
-            <li className="menu__item">
-              <a href="#4" className="menu__item-link">Women</a>
-            </li>
-            <li className="menu__item">
-              <a href="#5" className="menu__item-link">About</a>
-            </li>
-            <li className="menu__item">
-              <a href="#7" className="menu__item-link">Contact</a>
-            </li>
+        <ul className={`menu__list ${isMenuOpen ? "menu-mobile" : ""}`}>
+          <img onClick={() => closeMenu()} className="menu__close" src={MenuCloseIcon} alt="Menu Close Icon" />
+          <li className="menu__item">
+            <a href="#2" className="menu__item-link">Collections</a>
+          </li>
+          <li className="menu__item">
+            <a href="#1" className="menu__item-link">Men</a>
+          </li>
+          <li className="menu__item">
+            <a href="#4" className="menu__item-link">Women</a>
+          </li>
+          <li className="menu__item">
+            <a href="#5" className="menu__item-link">About</a>
+          </li>
+          <li className="menu__item">
+            <a href="#7" className="menu__item-link">Contact</a>
+          </li>
         </ul>
-        )
-      }
     </div>
   )
 }
